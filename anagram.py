42518d13a528d4f1179f63eb937e71c48a9a87fc
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-07 22:32:53 -0400

    string_utils & anagram written and tested

--- anagram.py
+++ anagram.py
@@ -2,5 +2,6 @@ def is_anagram(first_string: str, second_string: str) -> bool:
     """
     Given two strings, this functions determines if they are an anagram of one another.
     """
-    pass  # remove pass statement and implement me
-
+    # list_first = list(first_string).sort()
+    # list_second = list(second_string).sort()
+    return sorted(first_string) == sorted(second_string)
